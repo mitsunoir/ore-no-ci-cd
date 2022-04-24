b443cb8ce7d4a0839076161fa3946e2826dec781
Gyoza <mitsunori22@gmail.com>
2022-04-24 19:40:39 +0900

    add: main script

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mitsunoir/ore-no-ci-cd
+
+go 1.17
+
+require github.com/mitsunoir/ore-no-ci-cd/common v0.0.0-20220424102846-aecd59da076a // indirect
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "fmt"
+
+    "github.com/mitsunoir/ore-no-ci-cd/common"
+)
+
+func main() {
+    fmt.Println("Hello, World!")
+    fmt.Println("Add(4, 5) = ", common.Add(4, 5))
+}
