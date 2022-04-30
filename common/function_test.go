70e6cf3a386408b5ff319b4ebc4901693c193eaf
Gyoza <mitsunori22@gmail.com>
2022-04-30 19:00:05 +0900

    add: substract function

--- common/function_test.go
+++ common/function_test.go
@@ -5,6 +5,13 @@ import "testing"
 func TestAdd(t *testing.T) {
     want := 5
     if got := Add(2, 3); got != want {
-        t.Errorf("Add(2, 3) != %q, want %q", got, want)
+        t.Errorf("Add(2, 3) != %d, want %d", got, want)
+    }
+}
+
+func TestSub(t *testing.T) {
+    want := -1
+    if got := Sub(2, 3); got != want {
+        t.Errorf("Sub(2, 3) != %d, want %d", got, want)
     }
 }
